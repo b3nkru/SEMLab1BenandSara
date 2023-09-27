b0a9b35b94ad0b637d57e9fff819078c70e34086
Ben Kruseski <72771152+BKruseski@users.noreply.github.com>
2023-09-26 20:19:42 -0400

    Modified Circle Class and Added Shape Classes

--- Ben Kruseski & Sara Lawerence Lab 1/GeoShapes/src/geoshapes/Circle.java
+++ Ben Kruseski & Sara Lawerence Lab 1/GeoShapes/src/geoshapes/Circle.java
@@ -9,5 +9,29 @@ package geoshapes;
  * @author benkruseski
  */
 public class Circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
     
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    
+    public double getCircumference() {
+        return getPerimeter();
+    }
 }
