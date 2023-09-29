a3ac69f9f65c044f2a2314a9322ed8aef40dd41a
rxses0 <146390220+rxses0@users.noreply.github.com>
2023-09-28 22:10:30 -0400

    Update TriangularPrism.java

--- Ben Kruseski & Sara Lawerence Lab 1/GeoShapes/src/geoshapes/TriangularPrism.java
+++ Ben Kruseski & Sara Lawerence Lab 1/GeoShapes/src/geoshapes/TriangularPrism.java
@@ -8,6 +8,32 @@ package geoshapes;
  *
  * @author benkruseski
  */
-public class TriangularPrism {
+public class TriangularPrism extends Triangle {
+    private double height;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public TriangularPrism(double height, double side1, double side2, double side3) {
+        super(side1, side2, side3);
+        this.height = height;
+    }
+    
+    public double getVolume() {
+        return Math.round(super.getArea() * height);
+    }
+    
+    public double getLateralArea() {
+        return Math.round(super.getPerimeter() * height);
+    }
+    
+    public double getSurfaceArea() {
+        return Math.round(super.getPerimeter() * height) + (2 * super.getArea());
+    }
     
 }
